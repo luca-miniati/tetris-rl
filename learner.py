971af3ff224d9b82b8196a47d167f919760822e6
Luca Miniati <luca.miniati@gmail.com>
2023-07-13 17:21:31 -0700

    rename to TetrisLearner

--- learner.py
+++ learner.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim
 
 
-class Learner:
+class TetrisLearner:
     def __init__(self, model, lr, discount_factor):
         self.model = model
         self.lr = lr
